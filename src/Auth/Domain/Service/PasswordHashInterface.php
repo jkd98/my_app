3cfc8c8997abbe1827849d539990058a6cb5cb3f
jk <davidlara@aguida.com.mx>
2026-06-10 10:13:40 -0600

    feat: Agregando la interface para hashear la contraseña

---
+++ src/Auth/Domain/Service/PasswordHashInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Auth\Domain\Service;
+use App\Auth\Domain\ValueObject\Password;
+use App\Auth\Domain\ValueObject\RawPassword;
+
+interface PasswordHashInterface {
+    public function hash(RawPassword $rawPassword):Password; 
+    public function verify(RawPassword $rawPassword, Password $passwordHashed):bool;
+}
