6f6510aefbc86b5fc510b34cf04e4c23636eeb08
jk <davidlara@aguida.com.mx>
2026-07-03 16:46:41 -0600

    feat: Se configura el router principal

--- src/Shared/Infrastructure/Router/Router.php
+++ src/Shared/Infrastructure/Router/Router.php
@@ -1,2 +1,18 @@
 <?php
 
+namespace App\Shared\Infrastructure\Router;
+
+final class Router {
+    private static array $routers = [
+        "auth" => "App\Auth\Infrastructure\Router\AuthRouter"
+    ];
+
+    private function __construct(){}
+
+    public static function resolve(string $method, string $path): ?string {
+        $uri_parts = explode('/',$path);
+        $bounded_context = $uri_parts[1];
+        return self::$routers[$bounded_context]::router($method,$path) ?? null;
+    }
+
+}
