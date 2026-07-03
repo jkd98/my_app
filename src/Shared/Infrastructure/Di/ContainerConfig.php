2a4b4db2f38a6152c84212c90fa4657222308dc4
jk <davidlara@aguida.com.mx>
2026-07-02 21:18:09 -0600

    refactor: Se agregan los bindings para registrar a un usuario

--- src/Shared/Infrastructure/Di/ContainerConfig.php
+++ src/Shared/Infrastructure/Di/ContainerConfig.php
@@ -7,9 +7,30 @@ use App\Shared\Infrastructure\Di\Container;
 final class ContainerConfig {
     private function __construct() {}
 
-    public static function create(){
+    public static function create():Container{
         $container = new Container();
-        
+
+        $classToInstance = [
+            "App\Auth\Domain\Repository\UserRepositoryInterface" => "App\Auth\Infrastructure\Persistence\UserRepository",
+            "App\Auth\Domain\Repository\VerificationTokenRepositoryInterface" => "App\Auth\Infrastructure\Persistence\VerificationTokenRepository",
+            "App\Auth\Domain\Service\PasswordHashInterface" => "App\Auth\Infrastructure\Security\PasswordHash",
+            "App\Auth\Domain\Service\VerifyEmailExist" => "App\Auth\Domain\Service\VerifyEmailExist",
+            "App\Shared\Application\Port\EventDispatcherInterface" => "App\Auth\Infrastructure\EventDispatcher\EventDispatcher",
+            "App\Shared\Application\Port\TransactionManagerInterface" => "App\Shared\Infrastructure\Persistence\TransactionManager",
+            "PDO" => function(){ 
+                return new PDO(
+                    'mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_NAME').';charset=utf8mb4',
+                    getenv('DB_USER'),
+                    getenv('DB_PASSWORD')
+                );
+            }
+        ];
+
+        foreach($classToInstance as $key => $value){
+            $container->bind($key,$value);
+        }
+
+        return $container;
     }
 
 }
