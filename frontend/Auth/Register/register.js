081959a05bd12bcf25f5afa367cf7f532edd0204
jk <davidlara@aguida.com.mx>
2026-07-17 20:06:27 -0600

    refactor: Se crea un modulo compartido  para validar formularios

--- frontend/Auth/Register/register.js
+++ frontend/Auth/Register/register.js
@@ -1,26 +1,8 @@
+import { validations, validateField, isEquals } from "../../shared/form-validations.js";
 import { fetchAPI } from "../../shared/api.js"
 import { toast } from "../../shared/toast.js";
 
 window.document.addEventListener('DOMContentLoaded', init);
-
-const validations = {
-    NOT_NULL: (value, msg, options = null) => {
-        return /^\s*$/i.test(value) ? msg : null;
-    },
-    ONLY_LETTERS: (value, msg, options = null) => {
-        return /^[A-Za-záéíóúÁÉÍÓÚÑñ\s]*$/i.test(value) ? null : msg;
-    },
-    ONLY_NUMBERS: (value, msg, options = null) => {
-        return /^\d{1,}/.test(value) ? msg : null;
-    },
-    IS_EMAIL: (value, msg, options = null) => {
-        return /^[a-z0-9\.\_\+\-]+@[a-z0-9]+(\.[a-z]{2,}){1,}$/i.test(value) ? null : msg;
-    },
-    MIN_LENGTH: (value, msg, options = null) => {
-        let validOptions = options ? options : { min: 1 };
-        return value.length >= validOptions.min ? null : msg;
-    }
-}
 
 let userForm = {
     userName: {
@@ -86,16 +68,17 @@ function handleSubmit() {
         console.log(response);
         if (!response.ok && !response.status) {
             toast(3000, 'error', response.error);
-            return;
         }
 
         if (!response.ok && response.status) {
             toast(5000, 'warning', response.data.msg);
-            return;
         }
 
-        toast(5000, 'success', response.data.msg);
-        form.reset();
+        if(response.ok && response.status){
+            toast(5000, 'success', response.data.msg);
+            form.reset();
+        }
+
         spinner.classList.add('hidden');
         form.classList.remove('hidden');
 
@@ -108,49 +91,4 @@ function validateForm() {
         validateField(key, obj);
     });
     isEquals(userForm.rawPassword.element, userForm.rawPasswordRepeat.element, "Las contraseñas deben ser iguales");
-}
-
-function validateField(fieldName, fieldObj) {
-    let value = fieldObj.element.value;
-    let errors = [];
-    fieldObj.validations.forEach((validation, i) => {
-        let error = null;
-        if (fieldObj.options) {
-            error = validation(value, fieldObj.msgs[i], fieldObj.options[i])
-        } else {
-            error = validation(value, fieldObj.msgs[i])
-        }
-
-        if (error !== null) {
-            errors = [...errors, error];
-        }
-    })
-    renderMsgField(errors, fieldObj.element);
-}
-
-function renderMsgField(errors, element) {
-    let nodes = [...element.closest(".form__campo").getElementsByClassName('form__campo--error')];
-    if (nodes.length > 0) {
-        nodes.forEach((node) => {
-            node.remove();
-        })
-    }
-
-    if (errors.length > 0) {
-        errors.forEach((_, i) => {
-            element.classList.add('error');
-            let errorHTML = window.document.createElement("span");
-            errorHTML.classList.add('form__campo--error')
-            errorHTML.innerText = errors[i];
-            element.closest(".form__campo").appendChild(errorHTML);
-        })
-    } else {
-        element.classList.remove('error');
-    }
-}
-
-function isEquals(field, secondField, msg) {
-    if (field.value !== secondField.value) {
-        renderMsgField([msg], secondField);
-    }
 }
