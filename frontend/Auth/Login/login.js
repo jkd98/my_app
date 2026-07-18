dc0507c8a9c6207f7c9347f4278bd656f180c256
jk <davidlara@aguida.com.mx>
2026-07-17 21:08:15 -0600

    refactor: Se inicializan las variables que recuperan elementos del DOM hasta que este se dispare el evento DOMContentLoaded

--- frontend/Auth/Login/login.js
+++ frontend/Auth/Login/login.js
@@ -1,5 +1,28 @@
-window.document.addEventListener('DOMContentLoaded',init);
+import { validations } from "../../shared/form-validations.js";
+
+window.document.addEventListener('DOMContentLoaded', init);
+
+let loginForm = null;
+let spinner = null;
 
 function init() {
-    console.log("[DEBUG]: login loaded")
+    console.log("[DEBUG]: login loaded");
+    spinner = window.document.getElementById('spinner');
+    loginForm = {
+        email: {
+            element: window.document.getElementById('email'),
+            validations: [validations.NOT_NULL, validations.IS_EMAIL],
+            msgs: ["El email no puede ir vacío.", "Esto no parece un email."]
+        },
+        rawPassword: {
+            element: window.document.getElementById('rawPassword'),
+            validations: [validations.NOT_NULL],
+            msgs: ["La contraseña no puede ir vacía."]
+        }
+    }
+    handleSubmit();
+}
+
+function handleSubmit() {
+
 }
--- frontend/Auth/Register/register.js
+++ frontend/Auth/Register/register.js
@@ -4,39 +4,41 @@ import { toast } from "../../shared/toast.js";
 
 window.document.addEventListener('DOMContentLoaded', init);
 
-let userForm = {
-    userName: {
-        element: window.document.getElementById('userName'),
-        validations: [validations.NOT_NULL, validations.ONLY_LETTERS],
-        msgs: ["El nombre no puede ir vacío", "El nombre solo puede contener letras"]
-    },
-    lastName: {
-        element: window.document.getElementById('lastName'),
-        validations: [validations.NOT_NULL, validations.ONLY_LETTERS],
-        msgs: ["Este campo no puede ir vacío", "El apellido solo puede contener letras"]
-    },
-    email: {
-        element: window.document.getElementById('email'),
-        validations: [validations.NOT_NULL, validations.IS_EMAIL],
-        msgs: ["Este campo no puede ir vacío", "Esto no parece un email"]
-    },
-    rawPassword: {
-        element: window.document.getElementById('rawPassword'),
-        validations: [validations.NOT_NULL, validations.MIN_LENGTH],
-        msgs: ["Este campo no puede ir vacío", "La contraseña debe tener al menos 8 caracteres"],
-        options: [null, { min: 8 }]
-    },
-    rawPasswordRepeat: {
-        element: window.document.getElementById('rawPasswordRepeat'),
-        validations: [validations.NOT_NULL],
-        msgs: ["Este campo no puede ir vacío"]
-    }
-}
-let spinner = window.document.getElementById('spinner');
+let userForm = null;
+let spinner = null;
 
 
 function init() {
-    console.log("[DEBUG]: INICIO REGISTER SCRIPT")
+    console.log("[DEBUG]: INICIO REGISTER SCRIPT");
+    spinner = window.document.getElementById('spinner');
+    userForm = {
+        userName: {
+            element: window.document.getElementById('userName'),
+            validations: [validations.NOT_NULL, validations.ONLY_LETTERS],
+            msgs: ["El nombre no puede ir vacío", "El nombre solo puede contener letras"]
+        },
+        lastName: {
+            element: window.document.getElementById('lastName'),
+            validations: [validations.NOT_NULL, validations.ONLY_LETTERS],
+            msgs: ["Este campo no puede ir vacío", "El apellido solo puede contener letras"]
+        },
+        email: {
+            element: window.document.getElementById('email'),
+            validations: [validations.NOT_NULL, validations.IS_EMAIL],
+            msgs: ["Este campo no puede ir vacío", "Esto no parece un email"]
+        },
+        rawPassword: {
+            element: window.document.getElementById('rawPassword'),
+            validations: [validations.NOT_NULL, validations.MIN_LENGTH],
+            msgs: ["Este campo no puede ir vacío", "La contraseña debe tener al menos 8 caracteres"],
+            options: [null, { min: 8 }]
+        },
+        rawPasswordRepeat: {
+            element: window.document.getElementById('rawPasswordRepeat'),
+            validations: [validations.NOT_NULL],
+            msgs: ["Este campo no puede ir vacío"]
+        }
+    }
     handleSubmit();
     spinner.classList.add('hidden');
 }
@@ -53,7 +55,7 @@ function handleSubmit() {
             hasErrors = true;
             return;
         }
-        
+
         const dataToSend = {
             userName: userForm.userName.element.value,
             lastName: userForm.lastName.element.value,
@@ -63,7 +65,7 @@ function handleSubmit() {
         console.log("[DEBUG_DATA]: Data to send... " + JSON.stringify(dataToSend));
         spinner.classList.remove('hidden');
         form.classList.add('hidden');
-        
+
         const response = await fetchAPI('/auth/register', dataToSend, 'POST');
         console.log(response);
         if (!response.ok && !response.status) {
@@ -74,7 +76,7 @@ function handleSubmit() {
             toast(5000, 'warning', response.data.msg);
         }
 
-        if(response.ok && response.status){
+        if (response.ok && response.status) {
             toast(5000, 'success', response.data.msg);
             form.reset();
         }
